ce3a819589ff8805e69dd14420f95a967c57932f
Ben Doan <bendoan5@gmail.com>
2012-02-08 19:27:06 -0600

    moved functions to index

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 // Start the session
 session_start();
 
@@ -60,5 +59,51 @@ function redirect($location,$message=null) {
 		$_SESSION['message'] = $message;
 	}
 	header("Location:$location");
+}
+
+//returns the resource identifer matching the resource id param
+function getResourceDesc($id){
+    $conn = new mysqli('localhost',DB_USERNAME,DB_PASSWORD,DB_NAME);
+    $sql = "SELECT * FROM resources WHERE resource_id={$id}";
+    $results = $conn->query($sql);
+
+    $resource_description = null;
+    while($row = $results->fetch_assoc()){
+        $resource_description = $row['resource_identifier'];
+    }
+    return $resource_description;
+}
+
+//returns the username matching the id param
+function getUsername($id){
+    $conn = new mysqli('localhost',DB_USERNAME,DB_PASSWORD,DB_NAME);
+    $sql = "SELECT * FROM users WHERE user_id=$id";
+    $results = $conn->query($sql);
+
+    $username = "";
+    while($row = $results->fetch_assoc()){
+            $username = $row['user_username'];
+    }
+    $conn->close();
+    return $username;
+}
+
+//writes $line to the log file
+function writeLineToLog($line){
+    $logFile = "logFile.txt";
+    $fh = fopen($logFile, 'a') or die("can't open file");
+    $line .= "\n";
+    fwrite($fh, $line);
+    fclose($fh);
+}
+
+//returns returns an array containing the whole log file
+function readLog(){
+    $file = "logFile.txt";
+    $fh = fopen($file, 'r');
+    $data = fread($fh, filesize($file));
+    fclose($fh);
+    $data = explode("\n", $data);
+    return $data;
 }
 ?>
