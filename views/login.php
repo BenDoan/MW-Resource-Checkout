3bd57519b6eaa43d1918f9c6c4caa9b4a91acc16
BenDoan <ben@simcaster.net>
2012-03-18 18:48:06 -0500

    revamped login page

--- views/login.php
+++ views/login.php
@@ -1,17 +1,8 @@
 <h1>Login</h1>
 <form action="./?action=authenticate" method="post">
-	<table>
-		<tr>
-			<th>Username</th>
-			<td><input name="username" type="text" /></td>
-		</tr>
-		<tr>
-			<th>Password</th>
-			<td><input name="password" type="password" /></td>
-		</tr>
-		<tr>
-			<th></th>
-            <td><input class="btn" type="submit" value="Login" /></td>
-		</tr>
-	</table>
+    <fieldset>
+        <div class="clearfix"><input type="text" placeholder="Username" name="username" /></div>
+        <div class="clearfix"><input type="text" placeholder="Password" name="password" /></div>
+        <button class="btn btn-primary" type="submit">Sign in</button>
+    </fieldset>
 </form>
