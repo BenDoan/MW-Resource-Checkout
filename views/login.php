392d9eaeb4bb02a6d1adbee0cfbcc849510e0f82
BenDoan <ben@simcaster.net>
2012-03-18 18:48:51 -0500

    changed password field type to password

--- views/login.php
+++ views/login.php
@@ -2,7 +2,7 @@
 <form action="./?action=authenticate" method="post">
     <fieldset>
         <div class="clearfix"><input type="text" placeholder="Username" name="username" /></div>
-        <div class="clearfix"><input type="text" placeholder="Password" name="password" /></div>
+        <div class="clearfix"><input type="password" placeholder="Password" name="password" /></div>
         <button class="btn btn-primary" type="submit">Sign in</button>
     </fieldset>
 </form>
