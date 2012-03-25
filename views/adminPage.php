598622b4100c4aa27ccf3dc1d1c9ebb32a8f45ae
Ben Doan <bendoan5@gmail.com>
2012-03-24 21:47:51 -0500

    started working on settings page

--- views/adminPage.php
+++ views/adminPage.php
@@ -91,12 +91,14 @@ if (isset($type)) {
       <li class="<?php print $request ?>"><a href="#requests" data-toggle="tab">Requests</a></li>
       <li class="<?php print $resource ?>"><a href="#resources" data-toggle="tab">Resources</a></li>
       <li class="<?php print $log ?>"><a href="#log" data-toggle="tab">Admin Log</a></li>
+      <li class="<?php print $settings ?>"><a href="#settings" data-toggle="tab">Settings</a></li>
     </ul>
 
     <div class="tab-content">
         <div class="tab-pane <?php print $user ?>" id="users"><?php include('admin/users.php');?></div>
         <div class="tab-pane <?php print $request ?>" id="requests"><?php include('admin/requests.php');?></div>
         <div class="tab-pane <?php print $resource ?>" id="resources"><?php include('admin/resources.php');?></div>
+        <div class="tab-pane <?php print $settings ?>" id="settings"><?php include('admin/settings.php');?></div>
         <div class="tab-pane <?php print $log ?>" id="log"><?php include('admin/log.php');?></div>
 </div>
 </div>
