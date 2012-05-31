5b82be5397fd074f66803230546c1796ac4ab9bd
BenDoan <ben@simcaster.net>
2012-05-30 23:17:15 -0500

    added new schema

--- resourcecheckout.sql
+++ resourcecheckout.sql
@@ -2,8 +2,8 @@
 -- version 3.4.5
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: May 04, 2012 at 06:54 AM
+-- Host: localhost
+-- Generation Time: May 31, 2012 at 06:16 AM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `resourcecheckout`
 --
-CREATE DATABASE `resourcecheckout` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `resourcecheckout`;
 
 -- --------------------------------------------------------
 
@@ -35,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `comment_date` date NOT NULL,
   `comment_message` text NOT NULL,
   PRIMARY KEY (`comment_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `comments`
@@ -46,8 +44,7 @@ INSERT INTO `comments` (`comment_id`, `comment_resource_id`, `comment_user_id`, 
 (2, 1, 1, '0000-00-00', 'second comment'),
 (3, 1, 1, '2012-05-03', 'Comments yaya'),
 (4, 1, 1, '2012-05-03', 'There are damages on this laptop. Bad ones.'),
-(5, 1, 1, '2012-05-03', 'Commenting on stuff...'),
-(6, 1, 1, '2012-05-03', 'kafdlsfj');
+(5, 1, 1, '2012-05-03', 'Commenting on stuff...');
 
 -- --------------------------------------------------------
 
@@ -62,18 +59,15 @@ CREATE TABLE IF NOT EXISTS `resources` (
   `resource_identifier` varchar(50) NOT NULL,
   `resource_blocktype` enum('Half','Full') NOT NULL,
   PRIMARY KEY (`resource_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
 
 --
 -- Dumping data for table `resources`
 --
 
 INSERT INTO `resources` (`resource_id`, `resource_type`, `resource_details`, `resource_identifier`, `resource_blocktype`) VALUES
-(1, 'Computer Lab', '25 Computers', 'Room 234', 'Half'),
-(2, 'Laptop Cart', '15 Computers', 'Cart A', 'Full'),
-(4, 'Candy', 'Fifty Pieces', 'Package 2', 'Full'),
-(5, 'Laptop Cart', '20 Laptops', 'Cart B', 'Full'),
-(6, 'Computer Lab', '2 1/2 Computers', 'Room 199', 'Half');
+(1, '1', '25 Computers', 'Room 234', 'Half'),
+(4, '3', 'Fifty Pieces', 'Package 2', 'Full');
 
 -- --------------------------------------------------------
 
@@ -88,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `schedule` (
   `schedule_date` date NOT NULL,
   `schedule_block` int(11) NOT NULL,
   PRIMARY KEY (`schedule_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=40 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=68 ;
 
 --
 -- Dumping data for table `schedule`
@@ -101,10 +95,7 @@ INSERT INTO `schedule` (`schedule_id`, `schedule_resource_id`, `schedule_user_id
 (16, 3, 1, '2011-12-21', 1),
 (17, 3, 1, '2011-12-21', 2),
 (22, 1, 1, '2012-03-25', 31),
-(23, 6, 2, '2012-03-25', 22),
 (25, 4, 4, '2012-01-17', 1),
-(26, 2, 4, '2012-01-17', 1),
-(27, 2, 4, '2012-01-25', 1),
 (31, 1, 5, '2012-02-09', 11),
 (33, 4, 1, '2012-02-15', 1),
 (34, 1, 4, '2012-03-07', 11),
@@ -112,7 +103,10 @@ INSERT INTO `schedule` (`schedule_id`, `schedule_resource_id`, `schedule_user_id
 (36, 1, 4, '2012-03-15', 11),
 (37, 1, 4, '2012-03-14', 11),
 (38, 1, 4, '2012-03-20', 11),
-(39, 1, 1, '2012-05-04', 11);
+(39, 1, 1, '2012-05-04', 11),
+(51, 1, 5, '2012-05-11', 42),
+(50, 1, 5, '2012-05-11', 12),
+(46, 1, 5, '2012-05-09', 11);
 
 -- --------------------------------------------------------
 
@@ -153,7 +147,6 @@ CREATE TABLE IF NOT EXISTS `types` (
 
 INSERT INTO `types` (`type_id`, `type_name`) VALUES
 (1, 'Computer Lab'),
-(2, 'Laptop Cart'),
 (3, 'Candy');
 
 -- --------------------------------------------------------
@@ -168,18 +161,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_lastname` varchar(50) NOT NULL,
   `user_username` varchar(50) NOT NULL,
   `user_password` varchar(50) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_firstname`, `user_lastname`, `user_username`, `user_password`) VALUES
-(1, 'Tracy', 'Moody', 'tmoody', '5f4dcc3b5aa765d61d8327deb882cf99'),
-(2, 'Chris', 'Ramey', 'cramey', '02d8c4ac323c5df679077f020f170453'),
-(5, 'Ben', 'Doan', 'simcaster', '5f4dcc3b5aa765d61d8327deb882cf99'),
-(4, 'Ben', 'Doan', 'admin', '5f4dcc3b5aa765d61d8327deb882cf99');
+INSERT INTO `users` (`user_id`, `user_firstname`, `user_lastname`, `user_username`, `user_password`, `user_email`) VALUES
+(1, 'Tracy', 'Moody', 'tmoody', '5f4dcc3b5aa765d61d8327deb882cf99', 'tmoody@example.com'),
+(2, 'Chris', 'Ramey', 'cramey', '02d8c4ac323c5df679077f020f170453', 'cramey@example.com'),
+(5, 'Ben', 'Doan', 'simcaster', '5f4dcc3b5aa765d61d8327deb882cf99', 'ben@simcaster.net'),
+(4, 'Ben', 'Doan', 'admin', '5f4dcc3b5aa765d61d8327deb882cf99', 'ben@simcaster.net'),
+(8, 'First', 'Last', 'username', 'b01abf84324066bdb4eed4d5bf20f887', 'email@email.com');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
