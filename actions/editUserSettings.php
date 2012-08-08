5b2bf657e93b31216f6b4f0c8db599759b0b4b50
BenDoan <ben@simcaster.net>
2012-08-08 13:45:18 -0500

    removed the restriction preventing admins from changing their passwords

--- actions/editUserSettings.php
+++ actions/editUserSettings.php
@@ -4,30 +4,26 @@ extract($_SESSION['user']);
 $time = getTimestamp();
 
 
-if (isAdmin()) {
-    redirect("./?p=userSettings", "Sorry, you can't edit an admin's settings", 'alert-error');
-}else {
-    if (md5($curpass) == $_SESSION['user']['user_password']) {
-        sqlQuery("UPDATE users SET user_firstname='$firstname', user_lastname='$lastname', user_email='$email' WHERE user_id='$user_id'");
-        writeLineToLog("$time - $user_username - Updated user information");
-        updateSessionUser();
+if (md5($curpass) == $_SESSION['user']['user_password']) {
+    sqlQuery("UPDATE users SET user_firstname='$firstname', user_lastname='$lastname', user_email='$email' WHERE user_id='$user_id'");
+    writeLineToLog("$time - $user_username - Updated user information");
+    updateSessionUser();
 
-        if ($newpass != '' || $newpass2 != '') {
-            if ($newpass == $newpass2) {
-                $md5Pass = md5($newpass);
-                sqlQuery("UPDATE users SET user_password='$md5Pass' WHERE user_id='$user_id'");
-                writeLineToLog("$time - $user_username - Updated password");
-                updateSessionUser();
-                redirect('./?p=userSettings', 'Settings saved');
-            }else{
-                redirect('./?p=userSettings', 'The two passwords you have entered do not match', 'alert-error');
-            }
-        }else{
+    if ($newpass != '' || $newpass2 != '') {
+        if ($newpass == $newpass2) {
+            $md5Pass = md5($newpass);
+            sqlQuery("UPDATE users SET user_password='$md5Pass' WHERE user_id='$user_id'");
+            writeLineToLog("$time - $user_username - Updated password");
+            updateSessionUser();
             redirect('./?p=userSettings', 'Settings saved');
+        }else{
+            redirect('./?p=userSettings', 'The two passwords you have entered do not match', 'alert-error');
         }
     }else{
-        redirect('./?p=userSettings', 'You did not enter the correct password', 'alert-error');
+        redirect('./?p=userSettings', 'Settings saved');
     }
+}else{
+    redirect('./?p=userSettings', 'You did not enter the correct password', 'alert-error');
 }
 
 ?>
