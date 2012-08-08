36dfb8dd7d041d3b9a666291355db861b377bedc
BenDoan <ben@simcaster.net>
2012-08-08 13:55:13 -0500

    added logging for checkout setting changes

--- actions/updateAppSettings.php
+++ actions/updateAppSettings.php
@@ -2,9 +2,11 @@
     if (!isAdmin()) {
         redirect("./");
     }
+    $time = getTimestamp();
     sqlQuery("UPDATE settings SET setting_value='{$_POST['1']}'WHERE setting_id=1");
     sqlQuery("UPDATE settings SET setting_value='{$_POST['2']}'WHERE setting_id=2");
 
-    writeLineToLog("");
+    writeLineToLog("$time - Admin - Deleted user $user_name");
+
     redirect('./', 'Settings saved');
 ?>
