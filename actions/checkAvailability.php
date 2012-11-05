76707da2ce08d2c60554632fc9519a4c0f0773c1
Ben Doan <bendoan5@gmail.com>
2012-11-05 11:59:48 -0600

    started adding support for halfblocks reservations

--- actions/checkAvailability.php
+++ actions/checkAvailability.php
@@ -1,49 +1,105 @@
 <?php
+function convertHalfBlocks($block){
+    switch ($block) {
+        case 11:
+            return 1;
+            break;
+        case 12:
+            return 2;
+            break;
+        case 21:
+            return 3;
+            break;
+        case 22:
+            return 4;
+            break;
+        case 31:
+            return 5;
+            break;
+        case 32:
+            return 6;
+            break;
+        case 41:
+            return 7;
+            break;
+        case 42:
+            return 8;
+            break;
+
+        default:
+            break;
+    }
+}
 //./?action=checkAvailability&date=2012-31-10&type=1
 extract($_GET);
 $resourceList = array();
 $STH = sqlSelect("SELECT * FROM resources WHERE resource_type=$type");
+$blocktype = sqlSelectOne("SELECT * FROM types WHERE type_id='$type'", 'type_blocktype');
 while($row = $STH->fetch()) {
     $resourceList[] = $row['resource_id'];
 }
-//print "<br /><br />Resource List<br />";
-//printArray($resourceList);
 
-$resourceBlocks = array();
-foreach ($resourceList as $x) {
-    $resourceBlocks[$x] = array(true, true, true, true);
-}
-//print "<br /><br />Resource Blocks<br />";
-//printArray($resourceBlocks);
-
-$sqlResourceList = "(";
-for ($i = 0; $i < sizeof($resourceList); $i++) {
-    $sqlResourceList .= $resourceList[$i];
-    if ($i != sizeof($resourceList) - 1) {
-        $sqlResourceList .= ', ';
+if ($blocktype == 'full') {
+    $resourceBlocks = array();
+    foreach ($resourceList as $x) {
+        $resourceBlocks[$x] = array(true, true, true, true);
     }
-}
-$sqlResourceList .= ')';
-//print "<br /><br />sql resource list<br />";
-//printArray($sqlResourceList);
-$STH = sqlSelect("SELECT * FROM schedule WHERE schedule_date='$date' AND schedule_resource_id in $sqlResourceList");
-while($row = $STH->fetch()) {
-    $resourceBlocks[$row['schedule_resource_id']][$row['schedule_block'] - 1] = false;
-    //print $row['schedule_block'];
-}
-//print "<br /><br />resource blocks new<br />";
-//printArray($resourceBlocks);
 
-$returnArray = array();
-for ($i = 0; $i < 4; $i++) {
-    $countTaken = 0;
-    foreach ($resourceBlocks as $resource) {
-        if (!$resource[$i]) {
-            $countTaken++;
+    $sqlResourceList = "(";
+    for ($i = 0; $i < sizeof($resourceList); $i++) {
+        $sqlResourceList .= $resourceList[$i];
+        if ($i != sizeof($resourceList) - 1) {
+            $sqlResourceList .= ', ';
         }
     }
-    $returnArray[$i] = $countTaken != sizeof($resourceBlocks);
+    $sqlResourceList .= ')';
+    $STH = sqlSelect("SELECT * FROM schedule WHERE schedule_date='$date' AND schedule_resource_id in $sqlResourceList");
+    while($row = $STH->fetch()) {
+        $resourceBlocks[$row['schedule_resource_id']][$row['schedule_block'] - 1] = false;
+    }
+
+    $returnArray = array();
+    for ($i = 0; $i < 4; $i++) {
+        $countTaken = 0;
+        foreach ($resourceBlocks as $resource) {
+            if (!$resource[$i]) {
+                $countTaken++;
+            }
+        }
+        $returnArray[$i] = $countTaken != sizeof($resourceBlocks);
+    }
+}else {
+    //half blocktype
+
+    $resourceBlocks = array();
+    foreach ($resourceList as $x) {
+        $resourceBlocks[$x] = array(true, true, true, true, true, true, true, true);
+    }
+
+    $sqlResourceList = "(";
+    for ($i = 0; $i < sizeof($resourceList); $i++) {
+        $sqlResourceList .= $resourceList[$i];
+        if ($i != sizeof($resourceList) - 1) {
+            $sqlResourceList .= ', ';
+        }
+    }
+    $sqlResourceList .= ')';
+    $STH = sqlSelect("SELECT * FROM schedule WHERE schedule_date='$date' AND schedule_resource_id in $sqlResourceList");
+    while($row = $STH->fetch()) {
+        $block = convertHalfBlocks($row['schedule_block']);
+        $resourceBlocks[$row['schedule_resource_id']][$block - 1] = false;
+    }
+    $returnArray = array();
+    for ($i = 0; $i < 8; $i++) {
+        $countTaken = 0;
+        foreach ($resourceBlocks as $resource) {
+            if (!$resource[$i]) {
+                $countTaken++;
+            }
+        }
+        $returnArray[$i] = $countTaken != sizeof($resourceBlocks);
+    }
 }
 
-//printArray($returnArray);
+$returnArray = array($blocktype, $returnArray);
 print json_encode($returnArray);
--- views/calendar.php
+++ views/calendar.php
@@ -15,12 +15,21 @@
 </div>
 <div class="blockdiv" style="display:none;">
     <br />Choose the periods<br />
-    <div class="btn-group blocks" style="display:inline-block;">
+    <div class="btn-group fullblocks" style="display:none;">
         <a href="./?action=reserve" class="btn">1</a>
         <a href="./?action=reserve" class="btn">2</a>
         <a href="./?action=reserve" class="btn">3</a>
         <a href="./?action=reserve" class="btn">4</a>
-        <!-- ./?action=reserve&date=10/10/10&resource=1&block=1&new=1 -->
+    </div>
+    <div class="btn-group halfblocks" style="display:none;">
+        <a href="./?action=reserve" class="btn">1 1<sup>st</sup> half</a>
+        <a href="./?action=reserve" class="btn">1 2<sup>nd</sup> half</a>
+        <a href="./?action=reserve" class="btn">2 1<sup>st</sup> half</a>
+        <a href="./?action=reserve" class="btn">2 2<sup>nd</sup> half</a>
+        <a href="./?action=reserve" class="btn">3 1<sup>st</sup> half</a>
+        <a href="./?action=reserve" class="btn">3 2<sup>nd</sup> half</a>
+        <a href="./?action=reserve" class="btn">4 1<sup>st</sup> half</a>
+        <a href="./?action=reserve" class="btn">4 2<sup>nd</sup> half</a>
     </div>
 </div>
 <script type="text/javascript">
@@ -29,7 +38,6 @@ var type = "";
 function genReserverUrl(rType, block, date) {
     return "./?action=reserve&rType=" + rType + "&block=" + block + "&date=" + date;
 }
-
 
 $('.resource').change(function() {
     $('.datediv').css('display', 'block');
@@ -44,20 +52,38 @@ $('.date').change(function() {
         cache: false
     }).done(function(html) {
         var blockArray = $.parseJSON(html);
-        for ($i = 0; $i < blockArray.length; $i++) {
-            $('.blocks').children('a').each(function (){
-                if ($(this).text() == ($i + 1) && blockArray[$i] == false) {
-                    $(this).addClass('disabled');
+        if (blockArray[0] == 'full') {
+            for ($i = 0; $i < blockArray[1].length; $i++) {
+                $('.fullblocks').children('a').each(function (){
+                    if ($(this).text() == ($i + 1) && blockArray[1][$i] == false) {
+                        $(this).addClass('disabled');
+                    }
+                });
+            }
+            $('.fullblocks').children('a').each(function (){
+                if ($(this).hasClass('disabled')) {
+                    $(this).attr('href', "");
+                }else {
+                    $(this).attr('href', genReserverUrl(type, $(this).text(), date));
                 }
             });
-        }
-        $('.blocks').children('a').each(function (){
-            if ($(this).hasClass('disabled')) {
-                $(this).attr('href', "");
-            }else {
-                $(this).attr('href', genReserverUrl(type, $(this).text(), date));
+        }else {
+            for ($i = 0; $i < blockArray[1].length; $i++) {
+                $('.blocks').children('a').each(function (){
+                    if ($(this).text() == ($i + 1) && blockArray[1][$i] == false) {
+                        $(this).addClass('disabled');
+                    }
+                });
             }
-        });
+            $('.blocks').children('a').each(function (){
+                if ($(this).hasClass('disabled')) {
+                    $(this).attr('href', "");
+                }else {
+                    $(this).attr('href', genReserverUrl(type, $(this).text(), date));
+                }
+            });
+
+        }
     });
     $('.blockdiv').css('display', 'block');
 });
