57cc71bd12961dafa4ef6500fb9f3d21c11dde93
BenDoan <ben@simcaster.net>
2012-11-11 01:27:24 -0600

    improved performance

--- template.php
+++ template.php
@@ -6,10 +6,6 @@
 
         <link rel="icon" type="image/ico" href="favicon.ico" />
 
-		<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
-		<script type="text/javascript" src="js/jquery-ui-1.8.17.custom.min.js"></script>
-        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-		<script type='text/javascript' src='calendar/fullcalendar-1.5.2/fullcalendar/fullcalendar.min.js'></script>
 
         <link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
 
@@ -18,23 +14,12 @@
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
 		<link rel="stylesheet" type="text/css" href="js/css/custom-theme/datepicker.css"/>
 		<link rel="stylesheet" type="text/css" href="resource.css"/>
-		<script type="text/javascript" src="resourcecheckout.js.php"></script>
 
 
-        <script type="text/javascript">
-            $(".alert").alert();
-            $(function() {
-                $('input[name=date]').datepicker({dateFormat: 'mm/dd/yy', beforeShowDay:$.datepicker.noWeekends});
-                $('input[name=checkoutdate]').datepicker({
-                    dateFormat: 'mm/dd/yy',
-                    beforeShowDay:$.datepicker.noWeekends,
-                    minDate:0
-                });
-            });
-        </script>
 
         <!-- Created by Tracy Moody, Destiny Osbourne and Ben Doan with assistance from the Omaha Bytes Club-->
 	</head>
+    <?php flush(); ?>
 	<body>
 		<header>
 			<?php include('layout/header.php');?>
@@ -50,5 +35,21 @@
 		<div id="footer">
 			<?php include('layout/footer.php');?>
 		</div>
+		<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.8.17.custom.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+		<script type='text/javascript' src='calendar/fullcalendar-1.5.2/fullcalendar/fullcalendar.min.js'></script>
+		<script type="text/javascript" src="resourcecheckout.js.php"></script>
+        <script type="text/javascript">
+            $(".alert").alert();
+            $(function() {
+                $('input[name=date]').datepicker({dateFormat: 'mm/dd/yy', beforeShowDay:$.datepicker.noWeekends});
+                $('input[name=checkoutdate]').datepicker({
+                    dateFormat: 'mm/dd/yy',
+                    beforeShowDay:$.datepicker.noWeekends,
+                    minDate:0
+                });
+            });
+        </script>
 	</body>
 </html>
