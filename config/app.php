ab290222b53d9d1b840b113da4edf2459dcb6458
Ben Doan <ben@simcaster.net>
2013-01-29 11:36:56 -0600

    updated app settings

--- config/app.php
+++ config/app.php
@@ -2,5 +2,6 @@
 // Application constants
 define('DEFAULT_VIEW','calendar');
 define('DEFAULT_ADMIN_PANEL_ITEM', 'user');
-define('NUM_BLOCKS', getSettingValue('Number of Blocks'));
+define('NUM_BLOCKS', intval(getSettingValue('Number of Blocks')));
 define('APP_NAME', getSettingValue('Site Name'));
+define('DISP_BADGES', getSettingValue('Display table count badges'));
